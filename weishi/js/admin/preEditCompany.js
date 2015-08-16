48e5f837c5c44853c570b5e60575a18bbddf4abf
guowei.cgw <guowei.cgw@alibaba-inc.com>
2015-08-16 12:50:28 +0800

    增加地区功能

--- weishi/js/admin/preEditCompany.js
+++ weishi/js/admin/preEditCompany.js
@@ -1,5 +1,40 @@
+function StringBuilder(){
+	this.init();
+};
+//初始化StringBuilder类
+StringBuilder.prototype.init = function(){
+	this.array = [];
+};
+//追加数据到StringBuilder类
+StringBuilder.prototype.append = function(element){
+	this.array.push(element);
+};
+//转换成String
+StringBuilder.prototype.toString = function(){
+	return this.array.join("");
+};
+
 $(function(){
 	$("#edit_company_btn").click(function(){
 		$(".edit_company_form").submit();
 	});
+	
+	//获取省份和城市
+	$(".provinceId").click(function(){
+		$.post('http://www.malasong.com/json/getAllProvince',function(data){
+		  if(data){
+			  var context = new  StringBuilder();
+			  for(var i = 0; i < data.length; i++){
+			      context.append("<option value='");
+				  context.append(data[i].provinceId);
+				  context.append("'>");
+				  context.append(data[i].provinceName);
+				  context.append("</option>");
+			  }
+			  $(this).append(context.toString());
+		  }
+		}, "json");
+	});
 });
+
+
