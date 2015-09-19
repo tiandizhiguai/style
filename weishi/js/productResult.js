7452fc3983614d44c74bba5807652f41118f9eb9
guowei.cgw <guowei.cgw@alibaba-inc.com>
2015-09-19 10:57:33 +0800

    完善首页

--- weishi/js/productResult.js
+++ weishi/js/productResult.js
@@ -9,4 +9,28 @@ $(function(){
 		    thisObj.removeClass("product_hover");
 		});
 	});
+	
+	var $pageNo = parseInt($(".page_no").val());
+	var $totalPage = parseInt($(".total_page").val());
+	
+	$("#pre_page").click(function(){
+		var $prePage = 1;
+		if($pageNo > 1){
+		   $prePage = $pageNo - 1;
+		   $("#submit_page_no").val($prePage);
+		   $("#index_form").attr("action", "http://www.malasong.com/index").submit();
+		}else{
+		    $("#pre_page").class("disabled");
+		}
+	});
+	
+	$("#next_page").click(function(){
+		if($pageNo < $totalPage){
+		    var $nextPage = $pageNo + 1;
+			$("#submit_page_no").val($nextPage);
+			$("#index_form").attr("action", "http://www.malasong.com/index").submit();
+		}else{
+		    $("#next_page").class("disabled");
+		}
+	});
 });
