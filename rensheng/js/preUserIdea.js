5e03345b326793661d3fc56b15364a61da8bfa5b
guowei.cgw <guowei.cgw@alibaba-inc.com>
2016-02-21 12:26:13 +0800

    完善功能

---
+++ rensheng/js/preUserIdea.js
@@ -0,0 +1,33 @@
+function checkContent(content){
+	if(content == ""){
+		$("#check_cotent").empty().append("请输入反馈内容");
+		return false;
+	}
+	
+	if(content.length > 500){
+		$("#check_cotent").empty().append("不能超过500字");
+		return false;
+	}
+	
+	return true;
+}
+
+$(function(){
+	
+	$("#content").blur(function(){
+		checkContent($(this).val());
+	}).click(function(){
+	    $("#check_cotent").empty();
+	});
+	
+    $("#user_idea_btn").click(function(){
+	    if(checkContent($("#content").val())){
+		    $.post('//www.rensheng.com/json/addUserIdea',{'content': $("#content").val()},function(data){
+				if(!data.hasError){
+					$("#check_cotent").empty().append("感谢您的反馈，我们会尽快处理");
+					$("#content").val("");
+				}
+			}, "json");
+		}
+	});
+});
